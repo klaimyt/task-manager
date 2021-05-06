4e31c4c2969c11af0438571123a9587404685644
Klaimyt <vano-violinist@yandex.ru>
2021-05-06 21:18:00 +0300

    Make user task state update method. Fix verify token

--- Backend/routes/employee.js
+++ Backend/routes/employee.js
@@ -2,6 +2,8 @@ const express = require('express')
 const Task = require('../models/DB/Task')
 const verifyToken = require('../premissions/verifyToken')
 const canViewPage = require('../premissions/authorization')
+const STATE = require('../models/State')
+const ROLE = require('../models/Role')
 
 const router = express.Router()
 
@@ -15,9 +17,16 @@ router.get('/:userId', verifyToken, canViewPage, async (req,res) => {
     }
 })
 
-router.patch('/:userId/:taskId', verifyToken, canViewPage, (req, res) => {
-    const userData = res.locals.userData
-    const task = Task.findById(req.params.taskId)
+router.patch('/:userId/:taskId', verifyToken, canViewPage, async (req, res) => {
+    const newState = req.body.state
+    if (!Object.values(STATE).includes(newState)) return res.status(400).json({error: "Invalid state"})
+    if (newState === STATE.FINISHED && req.user.role === ROLE.EMPLOYEE) return res.status(405).json({ error: "Employee can't set finished status"})
+    await Task.findByIdAndUpdate(req.params.taskId, { state: newState }, { useFindAndModify: false })
+    .exec()
+    .then(result => {
+        res.status(200).send('Updated')
+    })
+    .catch(error => res.status(500).send(error))
 })
 
 module.exports = router
