4096c3455d5444839c69f09c00a9d02609981c16
Ivan Klimenkov <klimenkov.4game@gmail.com>
2021-05-08 16:21:22 +0300

    Clean employer router. (Forgot to save before git in last commit)

--- Backend/routes/employer.js
+++ Backend/routes/employer.js
@@ -5,24 +5,10 @@ const { isAdmin } = require('../premissions/authorization')
 
 const router = express.Router()
 
-async function canViewPage(req, res, next) {
-    res.locals.userData = await UserData.find({employerId: req.params.userId})
-
-    if (!res.locals.userData) return res.status(404).send("User not found")
-
-    if (res.locals.userData.employerId === req.params.userId ||
-        req.user.role === ROLE.ADMIN) {
-            next()
-    } else {
-        return res.status(401).send("Access denied")
-    }
-}
-
 // Get all availabel users
 router.get('/', verifyToken, (req, res) => {
     UserData.find({employerId: req.user._id}, (err, employees) => {
         if (err) return res.status(500).json(err)
-        console.log(req.user._id)
         res.json(employees)
     })
 })
