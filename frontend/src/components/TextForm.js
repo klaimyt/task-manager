b7b9cd5bf578e0a90807f46a778a22616ea34966
Klaimyt <vano-violinist@yandex.ru>
2021-05-16 12:46:34 +0300

    Add registration page

--- frontend/src/components/TextForm.js
+++ frontend/src/components/TextForm.js
@@ -5,7 +5,7 @@ const TextForm = (props) => {
   return (
     <div className={classes['text-form']}>
       <label htmlFor={props.inputId}>{props.labelText}</label>
-      <input type={props.inputType} id={props.inputId} />
+      <input value={props.inputValue} type={props.inputType} id={props.inputId} onChange={props.inputHandler} required minLength={props.minLength}/>
     </div>
   );
 };
