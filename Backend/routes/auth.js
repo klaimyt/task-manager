b6f2075bb3e1a17d25e9f369b31c8ffd89cb7bdc
Klaimyt <vano-violinist@yandex.ru>
2021-05-16 12:44:59 +0300

    Cookies for JWT. Add and setup cors

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -22,7 +22,8 @@ router.post('/login', async (req, res) => {
         if (!result) return res.status(400).json({ error: "Wrong username or password"})
         // JWT Token
         const token = jwt.sign({_id: user.id, role: user.role}, process.env.JWT_SECRET)
-        res.status(200).send(token)
+        res.cookie('access_token', 'Bearer ' + token,  {httpOnly: true, sameSite: 'strict', expires: new Date(Date.now() + 24 * 3600000) })
+        res.status(200).json({token: token})
     } catch (err) {
         res.status(500).json({error:err})
     }
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
 
@@ -10,6 +12,9 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopo
     .then(() => console.log('MongoDB Connected...'))
     .catch((err) => console.log(err))
 
+
+server.use(cors({credentials: true}))
+server.use(cookieParser())
 server.use(express.json())
 server.use('/api/auth', require('./routes/auth'))
 server.use('/api/employee', require('./routes/employee'))
