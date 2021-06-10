8f97568f9a43d0fcd4f98c0b4ca5f5d73a1f3ee4
Klaimyt <vano-violinist@yandex.ru>
2021-06-10 16:04:12 +0300

    Fix readability

--- frontend/src/components/ui/TextForm.js
+++ frontend/src/components/ui/TextForm.js
@@ -3,9 +3,18 @@ import classes from "./TextForm.module.css";
 
 const TextForm = (props) => {
   return (
-    <div className={classes['text-form']} style={props.style}>
+    <div className={classes["text-form"]} style={props.style}>
       <label htmlFor={props.inputId}>{props.labelText}</label>
-      <input autoComplete={props.autocomplete} onChange={props.onchange} autoFocus={props.autofocus} type={props.inputType} id={props.inputId} ref={props.inputRef} required minLength={props.minLength}/>
+      <input
+        autoComplete={props.autocomplete}
+        onChange={props.onchange}
+        autoFocus={props.autofocus}
+        type={props.inputType}
+        id={props.inputId}
+        ref={props.inputRef}
+        required
+        minLength={props.minLength}
+      />
     </div>
   );
 };
